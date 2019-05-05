cc0d12a11613218193f0feb3a89ad06695c34c1f
ramanaditya <adityaraman96@gmail.com>
2019-05-05 12:27:55 +0530

    Deleted glug.views

--- glug/urls.py
+++ glug/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from glug import views
+#from glug import views
 from django.conf import settings
 from django.conf.urls.static import static
 
--- glug/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from users import views
-from django.http import HttpResponse, HttpResponseRedirect
-# Create your views here.
-def index(request):
-    return HttpResponseRedirect('/users')
